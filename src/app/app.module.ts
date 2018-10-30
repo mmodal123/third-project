edd04e321fff397b35bf625b28a1c6e4156b4f68
mmodal123 <abdul.mmodal@gmail.com>
2018-10-30 06:55:15 +0530

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewModule } from './view/view.module';
 import { TestService } from './test.service';
+import 'hammerjs';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +18,7 @@ import { TestService } from './test.service';
     BrowserModule,
     HttpClientModule,
     ViewModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [TestService],
